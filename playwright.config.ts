f799f940458612b76c76115935a451718774f2a4
chandravasam1 <chandu.vasam92@gmail.com>
2026-07-03 18:13:53 +0530

    remove retry logic and no local server for html

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -35,8 +35,11 @@ export default defineConfig({
     headless:true,
   },
 metadata:{
-  appUsername:'narendraautomation@yopmail.com',
-  appPassword:'Test@1234567'
+  appUsername:'pwtest@nal.com',
+  appPassword:'test123'
+
+   // appUsername:'narendraautomation@yopmail.com',
+ // appPassword:'Test@1234567'
 },
   /* Configure projects for major browsers */
   projects: [
@@ -45,15 +48,15 @@ metadata:{
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
